d66bceab3b1dfee93ae22dbf1ed12bae5c8bcc9e
karan842 <karanshingde@gmail.com>
2025-03-01 01:52:33 +0530

    Updated experience a bit

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,7 +7,7 @@ const experiences = [
     title: "AI Engineer",
     company: "AiHello",
     period: "May 2024 - Present",
-    description: "Leading remote team and projects at AiHello Brain. Building innovative and exciting AI projects.",
+    description: "Leading remote team and projects at AiHello Brain. Building innovative and exciting AI projects. ",
   },
   {
     title: "AI Researcher & Technical Writer - Freelance",
@@ -19,19 +19,20 @@ const experiences = [
     title: "Machine Learning Engineer - Contract Based",
     company: "DataNNovite Sol",
     period: "Sep 2023 - May 2024",
-    description: "Built on MLOps infrastructure and part of R&D team.",
+    description: "Built and fine-tuned ML models, managed MLOps for real estate predictions, developed RAG pipelines, contributed to open-source NLP & CV models, and trained YOLOv9 for document verification.",
   },
+  
   {
     title: "Computer Vision Intern",
     company: "Netra AI",
     period: "Mar 2023 - Apr 2023",
-    description: "Worked on Langchain projects, but got laid off!",
+    description: "Worked on Langchain and data analytics projects but got laid off!",
   },
   {
     title: "Data Science Intern",
     company: "Markytics",
     period: "Aug 2022 - Oct 2022",
-    description: "Assisted team to build finance portal using Django and AWS.",
+    description: "Contributed to building a finance portal using Django and AWS. Transitioned out early to focus on AI and ML interests.",
   },
   {
     title: "Freelancer (Self-Employed)",
